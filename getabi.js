ebcf8b0b16424c0046efadbdc03aef94224b9792
piyush-trigun <34574118+piyush-trigun@users.noreply.github.com>
2018-02-11 00:14:24 +0530

    Delete getabi.js

--- getabi.js
+++
@@ -1,8 +0,0 @@
-fs = require('fs');
- Web3 = require('web3');
- web3 = new Web3( new Web3.providers.HttpProvider("http://localhost:8545/") );
- console.log("Connected to Blockchain !!");
- code = fs.readFileSync("Cards.sol").toString();
- solc = require('solc');
- compiledCode = solc.compile( code );
- abi = compiledCode.contracts[":Cards"].interface;
